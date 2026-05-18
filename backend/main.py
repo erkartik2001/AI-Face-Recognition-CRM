6e0a0a31da0c76c8ca7b4cfca098d488a70562ef
erkartik2001 <baderiyakartik24@gmail.com>
2026-05-18 12:46:42 +0530

    match images

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from backend.routes.search_routes import router as search_router
+
+app = FastAPI(
+    title= "AI Face Recognition API"
+)
+
+@app.get("/")
+def home():
+
+    return {
+        "message" :  "AI Face Recognition API Running"
+    }
+
+# Register routes
+app.include_router(search_router)
