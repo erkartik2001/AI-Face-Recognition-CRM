4fa7dea00402e9c5da20e9082f10198c5040bedc
erkartik2001 <baderiyakartik24@gmail.com>
2026-05-19 14:48:47 +0530

    new query image

--- scripts/download_test_image.py
+++ scripts/download_test_image.py
@@ -4,7 +4,7 @@ from backend.services.storage_service import B2Storage
 storage = B2Storage()
 
 
-file_name = "0-1-16283216dbacf18ff942c9440b3161d2.jpeg"
+file_name = "0ED566F5-A9DA-4BCF-817F-1B8251F8456D-0f483d838d9589f64382b9e278ebc052.jpeg"
 
 
 save_path = "query.jpg"
