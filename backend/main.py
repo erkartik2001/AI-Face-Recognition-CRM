68603204d266da6cbe46a7e71119ee8626782288
erkartik2001 <baderiyakartik24@gmail.com>
2026-05-19 11:22:15 +0530

    auth routes

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from backend.routes.search_routes import router as search_router
 from backend.routes.upload_routes import router as upload_router
+from backend.routes.auth_routes import router as auth_router
 
 app = FastAPI(
     title= "AI Face Recognition API"
@@ -17,3 +18,4 @@ def home():
 # Register routes
 app.include_router(search_router)
 app.include_router(upload_router)
+app.include_router(auth_router)
