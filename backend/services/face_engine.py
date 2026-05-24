0c5ca9aa83338e78362f71d7e4adddd35da35d1c
erkartik2001 <baderiyakartik24@gmail.com>
2026-05-24 11:04:27 +0530

    cv2 headless

--- backend/services/face_engine.py
+++ backend/services/face_engine.py
@@ -3,9 +3,10 @@
 # Generate embedding
 # Output 512-dimensional vector
 
-import cv2
+# import cv2
 import numpy as np
 from insightface.app import FaceAnalysis
+from PIL import Image
 
 class FaceEngine():
 
@@ -29,7 +30,8 @@ class FaceEngine():
         """
 
         # Read image
-        image = cv2.imread(image_path)
+        # image = cv2.imread(image_path)
+        image = Image.open(image_path).convert("RGB")
 
         if image is None:
             raise ValueError(f"Could not read image: {image_path}")
